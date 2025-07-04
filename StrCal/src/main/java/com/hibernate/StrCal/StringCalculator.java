9ecaf6f58d4661db663e4e33949260a593939112
utkaX <kaneriyautsav59@gmail.com>
2025-07-04 21:03:20 +0530

    Repo created with first test case

---
+++ StrCal/src/main/java/com/hibernate/StrCal/StringCalculator.java
@@ -0,0 +1,18 @@
+package com.hibernate.StrCal;
+
+/**
+ * Hello world!
+ *
+ */
+public class StringCalculator 
+{
+	public int add(String input)
+	{
+		return 0;
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ StrCal/src/test/java/com/hibernate/StrCal/StringCalculatorTest.java
@@ -0,0 +1,16 @@
+package com.hibernate.StrCal;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class StringCalculatorTest
+{
+   StringCalculator sc=new StringCalculator();
+   
+   
+   @Test void testEmptyStringReturnsZero() {
+	   assertEquals(0,sc.add(""));
+   }
+}
